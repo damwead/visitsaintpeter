eda1e4468e5d23b2f9cb717bbbc1c4f33f3d8e5c
damwead <mikhal3vsky@gmail.com>
2021-12-22 20:04:10 +0300

    cards design,fire init & smll stff

---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
+
+export default function Navbar() {
+  // const { user, username } = useContext(UserContext)
+
+  return (
+    <nav className='navbar'>
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">HOME</button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
