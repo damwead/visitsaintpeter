d5181741bd4ff271e4b4fad2c1ca7d523c7d9979
damwead <mikhal3vsky@gmail.com>
2021-12-26 00:14:36 +0300

    main page design

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,22 @@ const premier = null; // firestore.collection('posts').doc(premier) == true/fals
 export default function Home() {
   return (
     <main className={styles.container}>
+      <div className='premiere'>
+        <Image className='image-banner' src='/you-guilty-banner.jpeg' width="2700" height="1000" />
+
+        <div className='premiere-content'>
+          <Image className='image' src='/you-guilty-aswell.jpg' width="400" height="562" />
+            <div className='premiere-text'>
+              <Link href={"post"}>
+                <h1>Avant Garde</h1>
+              </Link>
+            
+              <p>Кобо Абэ - великий японский писатель 20 века. Его пьеса «Ты тоже виновен», была написана  в зрелом возрасте, после принесших ему успех  шедевров «Женщина в песках», «Чужое лицо» и «Сожжённая карта». </p>
+            </div>
+          </div>
+      </div>
+
+
       <div className='flex-container'>
         <div className='card'>
           <Image className='image' src='/avant.jpg' width="400" height="562" />
