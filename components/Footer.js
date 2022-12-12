9423dbdceb26a98ce631ca95453d1aa8ae07f1c1
damwead <mikhal3vsky@gmail.com>
2022-12-12 22:55:06 +0100

    new events

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <p className="footer-text"> &copy; АНО Культурный Мост 2021</p>
+      <p className="footer-text"> &copy; АНО Культурный Мост 2022</p>
     </footer>
   )
 }
