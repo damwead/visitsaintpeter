cf7cdaf7b8ae404c358843fc34fb822295375188
damwead <mikhal3vsky@gmail.com>
2026-02-23 18:13:05 +0100

    hook fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
-  const userData = useUserData();
   return (
     <>
       <Navbar />
